f84729b77690c30020f176e581da23b5c31e289e
harvyvasmar <harvy@vasmargroup.com>
2026-01-22 23:38:09 -0500

    fix config.json to config.js

---
+++ config/config.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD || "postgres",
+    database: process.env.DB_NAME || "mvp",
+    host: process.env.DB_HOST || "127.0.0.1",
+    port: process.env.DB_PORT || 5432,
+    dialect: "postgres",
+  },
+
+  test: {
+    username: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD || "postgres",
+    database: process.env.DB_NAME || "mvp_test",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: "postgres",
+  },
+
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+};
