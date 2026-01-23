58a34405c8628aa95ac0ce05f17ebba9777162f4
harvyvasmar <harvy@vasmargroup.com>
2026-01-23 14:53:21 -0500

    fix server.js

--- src/server.js
+++ src/server.js
@@ -2,17 +2,18 @@ require("dotenv").config();
 const app = require("./app");
 const { initDb } = require("./models");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const start = async () => {
   try {
     console.log("DATABASE_URL exists:", !!process.env.DATABASE_URL);
+    console.log("Listening on port:", PORT);
+
     await initDb();
 
     app.listen(PORT, "0.0.0.0", () => {
-      console.log(`API running on http://0.0.0.0:${PORT}`);
+      console.log(`API running on port ${PORT}`);
     });
-
   } catch (error) {
     console.error("DB init error:", error);
   }
