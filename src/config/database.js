ed2d460b8babfbd4d2fa7caa70a5ed6c9276f5e6
harvyvasmar <harvy@vasmargroup.com>
2026-01-22 22:58:40 -0500

    database config for railway

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,33 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  // PRODUCCIÓN (Railway)
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
-    logging: false
-  }
-);
+    protocol: "postgres",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
+} else {
+  // DESARROLLO LOCAL
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: "postgres",
+      logging: false,
+    }
+  );
+}
 
 module.exports = sequelize;
