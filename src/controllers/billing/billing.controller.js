1b69a39721fa34a56ea8cd098600cf249c6d9b14
harvyvasmar <harvy@vasmargroup.com>
2026-02-25 12:27:23 -0500

    accestance token 500

--- src/controllers/billing/billing.controller.js
+++ src/controllers/billing/billing.controller.js
@@ -13,9 +13,11 @@ async function getAcceptanceToken(req, res) {
 
     return res.status(200).json({ acceptance_token: token });
   } catch (error) {
-    return res
-      .status(500)
-      .json({ message: "Error getting acceptance token" });
+    console.error("WOMPI ERROR:", error.response?.data || error.message);
+    return res.status(500).json({
+      message: "Error getting acceptance token",
+      error: error.response?.data || error.message,
+    });
   }
 }
 
